af545703105ebf6234e835fae53d4daef031e1b3
Mran <amr.bin.an@gmail.com>
2024-12-07 14:00:31 +0700

    update config again

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -14,12 +14,12 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-    }
-  }
+//  buildTypes {
+//    release {
+//      isMinifyEnabled = false
+//      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+//    }
+//  }
 
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_17
